c53134fc066fccc4d51192d921410c2f7b827533
Victoria-Butrym <victoria-butrym@tut.by>
2020-02-08 13:18:28 +0300

    feat: implement next level on click

--- songbird/src/App.js
+++ songbird/src/App.js
@@ -3,34 +3,48 @@ import "./App.css";
 import "./header/header.css";
 
 import Score from "./header/score";
+import birdsData from "./data/birds";
 
-class Answers extends Component {
-  render() {
-    return (
-      <Fragment>
-        <aside className="answers"></aside>
-      </Fragment>
-    );
-  }
-}
+const createAnswers = props => {
+  return (
+    <Fragment>
+      <ul className="possible-answers">
+        {props.map(answer => {
+          return <li className="possible-answer">{answer.name}</li>;
+        })}
+      </ul>
+    </Fragment>
+  );
+};
 
-class NextLevel extends Component {
-  render() {
-    return <button className="next">Next Level</button>;
-  }
-}
-
-// const Score = ({ score }) => {
-//   return <span className="score">Score: {score}</span>;
-// };
+const NextLevel = ({ onClick }) => {
+  return (
+    <button onClick={onClick} className="next">
+      Next Level
+    </button>
+  );
+};
 
 class App extends Component {
   state = {
-    score: 0
+    score: 0,
+    next: 1,
+    possibleAnswers: birdsData[0],
+    guessed: false
+  };
+
+  nextCategory = () => {
+    const { next } = this.state;
+    if (next === 6) return;
+    this.setState({
+      possibleAnswers: birdsData[next],
+      next: next + 1
+    });
   };
 
   render() {
-    const { score } = this.state;
+    const { score, possibleAnswers } = this.state;
+    console.log(this.state);
     return (
       <div className="App">
         <header className="App-header">
@@ -45,10 +59,10 @@ class App extends Component {
         <main>
           <section className="current-bird-section"></section>
           <section className="answers-section">
-            <Answers />
+            <aside className="answers">{createAnswers(possibleAnswers)}</aside>
             <aside className="current-answer"></aside>
           </section>
-          <NextLevel />
+          <NextLevel onClick={this.nextCategory} />
         </main>
       </div>
     );
@@ -56,3 +70,5 @@ class App extends Component {
 }
 
 export default App;
+
+// TODO: App component renders several times, so you're not able to create <li> answers from fetch
