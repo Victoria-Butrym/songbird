5aafdebd2ee7a034d2582c4ac8ec982e355bb6a7
Victoria-Butrym <victoria-butrym@tut.by>
2020-02-08 14:10:47 +0300

    feat: implement changing categories

--- songbird/src/App.js
+++ songbird/src/App.js
@@ -1,16 +1,48 @@
 import React, { Component, Fragment } from "react";
 import "./App.css";
 import "./header/header.css";
+import "./assets/categories.css";
 
 import Score from "./header/score";
 import birdsData from "./data/birds";
+import categories from "./data/categories";
+
+const Logo = () => {
+  return (
+    <Fragment>
+      <span className="song">
+        Song<span className="bird">bird</span>
+      </span>
+    </Fragment>
+  );
+};
+
+const Categories = ({ activeCategory }) => {
+  return (
+    <nav className="header-nav">
+      <ul className="categories">
+        {categories.map(category => {
+          if (category === activeCategory) {
+            return <li className="active-category">{category}</li>;
+          }
+          return <li className="category">{category}</li>;
+        })}
+      </ul>
+    </nav>
+  );
+};
 
 const createAnswers = props => {
   return (
     <Fragment>
       <ul className="possible-answers">
         {props.map(answer => {
-          return <li className="possible-answer">{answer.name}</li>;
+          return (
+            <li className="possible-answer">
+              <div className="unactive"></div>
+              {answer.name}
+            </li>
+          );
         })}
       </ul>
     </Fragment>
@@ -29,6 +61,7 @@ class App extends Component {
   state = {
     score: 0,
     next: 1,
+    activeCategory: categories[0],
     possibleAnswers: birdsData[0],
     guessed: false
   };
@@ -38,23 +71,22 @@ class App extends Component {
     if (next === 6) return;
     this.setState({
       possibleAnswers: birdsData[next],
+      activeCategory: categories[next],
       next: next + 1
     });
   };
 
   render() {
-    const { score, possibleAnswers } = this.state;
+    const { score, possibleAnswers, activeCategory } = this.state;
     console.log(this.state);
     return (
       <div className="App">
         <header className="App-header">
           <div className="logo-and-score">
-            <span className="song">
-              Song<span className="bird">bird</span>
-            </span>
+            <Logo />
             <Score score={score} />
           </div>
-          <nav className="header-nav"></nav>
+          <Categories activeCategory={activeCategory} />
         </header>
         <main>
           <section className="current-bird-section"></section>
